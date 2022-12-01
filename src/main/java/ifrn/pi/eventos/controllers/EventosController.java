f4c5e1397d22cfa814def4fe2d50382b56ed6e21
unknown <robson.cosme@escolar.ifrn.edu.br>
2022-12-01 14:23:12 -0300

    Atualizando layout da aplicação

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -16,7 +16,7 @@ public class EventosController {
 	
 	@RequestMapping("/eventos/form")
 	public String form() {
-		return "formEvento";
+		return "eventos/formEvento";
 	}
 
 	@RequestMapping( path = "/eventos" , method = RequestMethod.POST)
@@ -25,6 +25,6 @@ public class EventosController {
 		System.out.println(evento);
 		er.save(evento);
 		
-		return "evento-adicionado";
+		return "eventos/evento-adicionado";
 	}
 }
